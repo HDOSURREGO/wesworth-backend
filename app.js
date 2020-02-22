850da44bb7dcc58f9b1e6b980dd8542af850c851
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2020-02-22 18:35:11 -0500

    routes fixed

--- app.js
+++ app.js
@@ -1,65 +1,63 @@
-require("dotenv").config();
-const cors = require("cors");
+require('dotenv').config()
+const cors = require('cors')
 
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const favicon = require("serve-favicon");
-const hbs = require("hbs");
-const mongoose = require("mongoose");
-const logger = require("morgan");
-const path = require("path");
-const router = express.Router();
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const favicon = require('serve-favicon')
+const hbs = require('hbs')
+const mongoose = require('mongoose')
+const logger = require('morgan')
+const path = require('path')
+const router = express.Router()
 
 mongoose
-	.connect("mongodb://localhost/wesworth-backend", { useNewUrlParser: true })
-	.then(x => {
-		console.log(
-			`Connected to Mongo! Database name: "${x.connections[0].name}"`
-		);
-	})
-	.catch(err => {
-		console.error("Error connecting to mongo", err);
-	});
+  .connect('mongodb://localhost/wesworth-backend', { useNewUrlParser: true })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  })
 
-const app_name = require("./package.json").name;
-const debug = require("debug")(
-	`${app_name}:${path.basename(__filename).split(".")[0]}`
-);
+const app_name = require('./package.json').name
+const debug = require('debug')(
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
+)
 
-const app = express();
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 // Middleware Setup
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 // Express View engine setup
 
 app.use(
-	require("node-sass-middleware")({
-		src: path.join(__dirname, "public"),
-		dest: path.join(__dirname, "public"),
-		sourceMap: true
-	})
-);
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
+  })
+)
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 // default value for title local
-app.locals.title = "Wesworth Backend";
+app.locals.title = 'Wesworth Backend'
 
-const index = require("./routes/index");
-app.use("/", index);
+const index = require('./routes/index')
+app.use('/', index)
 
-app.use("/electric/send", require("./routes/forms-routes"));
+app.use('/', require('./routes/forms-routes'))
 
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
+app.use('/', router)
 
-module.exports = app;
+module.exports = app
