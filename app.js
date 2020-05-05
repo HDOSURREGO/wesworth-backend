6be1483920a13f9ca9447a663a0a17ea2cc0923b
Henry Hoyos <hdosurrego@gmail.com>
2020-05-05 13:06:35 -0400

    sass commented out and worked

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ const router = express.Router();
 
 mongoose
 	.connect(process.env.MONGODB_URI, {
-		useNewUrlParser: true
+		useNewUrlParser: true,
 	})
-	.then(x => {
+	.then((x) => {
 		console.log(
 			`Connected to Mongo! Database name: "${x.connections[0].name}"`
 		);
 	})
-	.catch(err => {
+	.catch((err) => {
 		console.error("Error connecting to mongo", err);
 	});
 const app_name = require("./package.json").name;
@@ -39,18 +39,18 @@ app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(
-	require("node-sass-middleware")({
-		src: path.join(__dirname, "public"),
-		dest: path.join(__dirname, "public"),
-		sourceMap: true
-	})
-);
+// app.use(
+// 	require("node-sass-middleware")({
+// 		src: path.join(__dirname, "public"),
+// 		dest: path.join(__dirname, "public"),
+// 		sourceMap: true,
+// 	})
+// );
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "hbs");
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
 app.locals.title = "Wesworth Backend";
